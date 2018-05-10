528ae97767cc914bd8b6b5db495b97943c442443
fadi <fadi mabsaleh>
2018-05-10 16:29:15 +0300

    make it add fron /add

--- connect.js
+++ connect.js
@@ -9,13 +9,12 @@ let db = new sqlite3.Database('mydatabase.db', sqlite3.OPEN_READWRITE, (err) => 
 });
 
 // INSERT (add) one element to movies
-const insertOne = (name, genre) => {
-    console.log(name + " " + genre)
+const insertOne = (name, genre, results) => {
     db.run(`INSERT INTO movies(name, genre) VALUES(?, ?)`, [name, genre], function (err) {
         if (err) {
-            return console.log(err.message);
+            return results(err.message);
         }
-        console.log(`A row has been inserted with rowid ${this.lastID}`)
+        results(`A row has been inserted with rowid ${this.lastID}`)
     })
 }
 
@@ -41,16 +40,34 @@ const deleteOne = () => {
     })
 }
 
-// select all elemnts from movies table
-const mapAllTable = () => {
+// SELECT all elemnts from movies table
+const mapAllTable = (callback) => {
     db.all('SELECT * FROM movies', (err, rows) => {
         if (err) {
             console.log(err)
         }
+        let moviesHTML = '<ul>'
         rows.forEach((row) => {
+            moviesHTML += '<li>' + (row.id + '\t' + row.name + '\t' + row.genre) + '</li>'
             // console.log(row.id + '\t' + row.name + '\t' + row.genre)
             console.log(row)
         })
+        moviesHTML += '</ul>'
+        const page = `<!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <meta http-equiv="X-UA-Compatible" content="ie=edge">
+            <title>Document</title>
+        </head>
+        <body>
+            <div class="main">
+            `+ moviesHTML + `
+            </div>
+        </body>
+        </html>`
+        callback(page)
     })
 }
 
@@ -84,40 +101,47 @@ if (command === "--add") {
     } else {
         insertOne(name, genre)
     }
-}else if (command === "--list"){
+} else if (command === "--list") {
     mapAllTable()
 }
-shutdownDB()
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// 
+// 
 //               EXPRESS PART           /////////
+// 
+// 
 
-// const express = require('express');
-// const bodyParser = require('body-parser')
-// const app = express();
+const express = require('express');
+const bodyParser = require('body-parser')
+const app = express();
 
-// app.get('/', (req, res) => {
-//     res.send('./index.html')
-// })
+// path HOME page and render all movies ===> http://localhost:3000
+app.get('/', (req, res) => {
+    mapAllTable(function (rows) {
+        console.log('there are the rows again')
+        html = rows
+        res.send(html)
+    })
+})
 
+// path ADD to add more movie to the list ===> http://localhost:3000/add?name=<NAME>&genre=<GENRE>
+app.get('/add', (req, res) => {
+    const name = req.query.name;
+    const genre = req.query.genre;
+    if (!name){
+        res.send('name is required')
+    }else if (!genre){
+        res.send('genre is required')
+    }
+    insertOne(name, genre, function(message){
+        res.send(message)
+    })
+})
 
-// // Runnig the server 
-// const port = 3000; // listing port
-// app.listen(port, () => {
-//     console.log('Listening on port: ' + port)
-// })
+// Runnig the server 
+const port = 3000; // listing port
+app.listen(port, () => {
+    console.log('Listening on port: ' + port)
+})

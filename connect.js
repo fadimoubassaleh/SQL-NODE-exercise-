43a6bd9fefdcd40f37239114d0f8f1a1367e7c7b
fadi <fadi mabsaleh>
2018-05-10 14:44:02 +0300

    make all in functions

--- connect.js
+++ connect.js
@@ -9,49 +9,64 @@ let db = new sqlite3.Database('mydatabase.db', sqlite3.OPEN_READWRITE, (err) => 
 });
 
 // INSERT (add) one element to movies
-// db.run('INSERT INTO movies(id, name, genre) VALUES(7, "newname", "newgenre")', function (err) {
-//     if (err) {
-//         return console.log(err.message);
-//     }
-//     console.log(`A row has been inserted with rowid ${this.lastID}`)
-// })
+const insertOne = () => {
+    db.run('INSERT INTO movies(id, name, genre) VALUES(7, "newname", "newgenre")', function (err) {
+        if (err) {
+            return console.log(err.message);
+        }
+        console.log(`A row has been inserted with rowid ${this.lastID}`)
+    })
+}
 
 // UPDATE one the elemnts from movies
-// let data = ['hello my friends', 6];
-// let sql = `UPDATE movies SET name=? WHERE id=?`
-// db.run(sql, data, function(err){
-//     if (err){
-//         return console.error(err.message);
-//     }
-//     console.log(`it's updated`)
-// })
+const updateOne = () => {
+    let sql = `UPDATE movies SET name=? WHERE id=?`
+    let data = ['hello my friends', 6];
+    db.run(sql, data, function (err) {
+        if (err) {
+            return console.error(err.message);
+        }
+        console.log(`it's updated`)
+    })
+}
 
 // DELETE one of the elemnts from movies
-db.run(`DELETE FROM movies WHERE id=?`, 7, function(err){
-    if (err){
-        return console.error(err.message)
-    }
-    console.log(`DELETE is work`)
-})
+const deleteOne = () => {
+    db.run(`DELETE FROM movies WHERE id=?`, 7, function (err) {
+        if (err) {
+            return console.error(err.message)
+        }
+        console.log(`DELETE is work`)
+    })
+}
 
 // select all elemnts from movies table
-db.all('SELECT * FROM movies', (err, rows) => {
-    if (err) {
-        console.log(err)
-    }
-    rows.forEach((row) => {
-        // console.log(row.id + '\t' + row.name + '\t' + row.genre)
-        console.log(row)
+const mapAllTable = () => {
+    db.all('SELECT * FROM movies', (err, rows) => {
+        if (err) {
+            console.log(err)
+        }
+        rows.forEach((row) => {
+            // console.log(row.id + '\t' + row.name + '\t' + row.genre)
+            console.log(row)
+        })
     })
-})
+}
 
 
 
 
 // close the database connection
-db.close((err) => {
-    if (err) {
-        return console.error(err.message);
-    }
-    console.log('Close the database connection.');
-});
+const shutdownDB = ()=>{
+    db.close((err) => {
+        if (err) {
+            return console.error(err.message);
+        }
+        console.log('Close the database connection.');
+    });
+}
+
+insertOne();
+updateOne();
+deleteOne();
+shutdownDB();

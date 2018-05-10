09a5c09f2db9d9f1d8a93c18d7888083d10fc568
fadi <fadi mabsaleh>
2018-05-10 12:39:38 +0300

    update & delete

--- connect.js
+++ connect.js
@@ -8,22 +8,45 @@ let db = new sqlite3.Database('mydatabase.db', sqlite3.OPEN_READWRITE, (err) => 
     console.log('Connected to the in-memory SQlite database.');
 });
 
+// INSERT (add) one element to movies
+// db.run('INSERT INTO movies(id, name, genre) VALUES(7, "newname", "newgenre")', function (err) {
+//     if (err) {
+//         return console.log(err.message);
+//     }
+//     console.log(`A row has been inserted with rowid ${this.lastID}`)
+// })
+
+// UPDATE one the elemnts from movies
+// let data = ['hello my friends', 6];
+// let sql = `UPDATE movies SET name=? WHERE id=?`
+// db.run(sql, data, function(err){
+//     if (err){
+//         return console.error(err.message);
+//     }
+//     console.log(`it's updated`)
+// })
+
+// DELETE one of the elemnts from movies
+db.run(`DELETE FROM movies WHERE id=?`, 7, function(err){
+    if (err){
+        return console.error(err.message)
+    }
+    console.log(`DELETE is work`)
+})
+
+// select all elemnts from movies table
 db.all('SELECT * FROM movies', (err, rows) => {
     if (err) {
         console.log(err)
     }
     rows.forEach((row) => {
-        console.log(row.id + '\t' + row.name + '\t' + row.genre)
+        // console.log(row.id + '\t' + row.name + '\t' + row.genre)
         console.log(row)
     })
 })
 
-db.run('INSERT INTO movies(id, name, genre) VALUES(6, "newname", "newgenre")', function(err){
-    if(err){
-        return console.log(err.message);
-    }
-    console.log('A row has been inserted with rowid ')
-})
+
+
 
 // close the database connection
 db.close((err) => {
